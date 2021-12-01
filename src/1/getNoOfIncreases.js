a4c1ed03d2af59fe0bad98086f563cbace5a02a8
nataliea <natalie.akam@thetrainline.com>
2021-12-01 22:51:39 +0000

    Refactor D1P2 to oneliner :)

--- src/1/getNoOfIncreases.js
+++ src/1/getNoOfIncreases.js
@@ -4,12 +4,7 @@ const getNoOfIncreases = (data) => data.reduce(
 );
 
 export const getNoOfSlidingIncreases = (data) => data.reduce(
-    (acc, curr, currIndex) => {
-        if (!data[currIndex + 3]) return acc;
-        const currAverage = (curr + data[currIndex + 1] + data[currIndex + 2]) / 3;
-        const nextAverage = (data[currIndex + 1] + data[currIndex + 2] + data[currIndex + 3]) / 3;
-        return nextAverage > currAverage ? acc + 1 : acc;
-    }, 0
+    (acc, curr, currIndex) => data[currIndex + 3] > curr ? acc + 1 : acc, 0
 );
 
 export default getNoOfIncreases;
